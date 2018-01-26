b3a8e4079b26c03182b51e587c410f9216e0dcfe
kevin jiang <kevinjiang2009@yahoo.com>
2018-01-26 02:16:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 } 
 
-function holidayCountdown(days, holiday) {
+function holidayCountn(days, holiday) {
   return `It's ${days} days until ${holiday}!`
 }
 
